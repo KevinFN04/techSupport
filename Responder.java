3ae321a27563c0db73babe74c85ad1682e7e189a
KevinFN04 <kevin.felix.04@gmail.com>
2016-03-04 11:12:24 +0100

    Creados los atributos de tipo Random y ArrayList en la clase responder y recodificado el metodo generateResponse() para que de una respuesta de la lista de Strings inicializada en el constructor junto con el atributo Random

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,20 @@
  */
 public class Responder
 {
+    private Random nAleat;
+    private ArrayList<String> listaStrings;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        nAleat = new Random();
+        listaStrings = new ArrayList<>();
+        listaStrings.add ("Yeah.");
+        listaStrings.add("No, that's imposible.");
+        listaStrings.add("Of course.");
+        listaStrings.add("Your problem is my problem...");
+        listaStrings.add("I can't help you, sorry...");
     }
 
     /**
@@ -20,6 +31,6 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        return listaStrings.get(nAleat.nextInt(listaStrings.size()));
     }
 }
